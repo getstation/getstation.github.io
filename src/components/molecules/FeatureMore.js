e42e2dd2e3fb1899f68361992323b6119eb83783
sutter <laurent@sutterlity.fr>
2019-01-04 17:46:30 +0100

    🔥 REMOVE - Features animations

--- src/components/molecules/FeatureMore.js
+++ src/components/molecules/FeatureMore.js
@@ -73,11 +73,7 @@ const FeatureMore = ({ data, ...rest }) => {
       {data && (
         <Grid>
           {data.map((item, index) => (
-            <Box
-              key={`feature-more-${index}`}
-              data-aos="fade-up"
-              data-aos-delay={index * 100}
-            >
+            <Box key={`feature-more-${index}`}>
               {item.image.url && (
                 <img
                   className={css`
