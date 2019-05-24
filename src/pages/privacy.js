1f34e6dc2be0d5db6e08e60dac944f62bd715bd0
Mikaël <lorack@gmail.com>
2019-05-24 19:01:22 +0200

    Create a new page for managing privacy

---
+++ src/pages/privacy.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { graphql } from "gatsby";
+
+import AppMinimal from '../components/layout/AppMinimal';
+import Seo from '../components/molecules/Seo';
+
+import PrivacyBox from '../components/organims/PrivacyBox';
+
+class Privacy extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Init State
+    this.state = {
+      isClient: false,
+    };
+
+  }
+
+  componentDidMount() {
+    this.setState({
+      isClient: true,
+    });
+  }
+
+  render() {
+    const { isClient } = this.state;
+
+    if (isClient) {
+      return (
+        <AppMinimal>
+          <Seo
+            title="Station • Privacy access"
+            description="Manage your personal data on Station"
+          />
+          <PrivacyBox></PrivacyBox>
+        </AppMinimal>
+      );
+    }
+
+    return (
+      <p>Loading...</p>
+    );
+  }
+}
+
+export default Privacy;
