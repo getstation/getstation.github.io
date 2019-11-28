3a9333a485c2d12b7ee4520319e08e5529ad5b7c
MatthD <matthias.dieudonne@gmail.com>
2019-11-28 14:56:41 +0100

    page is now responsive & work with all data

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -8,6 +8,7 @@ import gql from 'graphql-tag';
 import AppMinimal from '../components/layout/AppMinimal';
 import Seo from '../components/molecules/Seo';
 import SectionFullPage from '../components/molecules/SectionFullPage';
+import SectionMinimal from '../components/molecules/SectionMinimal';
 import Button from '../components/atoms/Button';
 
 import { httpLink } from '../utils/apollo';
@@ -27,6 +28,8 @@ class WelcomeByOrg extends React.Component{
     this.state = {
       title: 'Your organization name',
       description: 'Your organization description',
+      details: 'Your organization details',
+      pictureUrl: '',
     }
   }
   componentDidMount(){
@@ -37,40 +40,50 @@ class WelcomeByOrg extends React.Component{
       variables: { slug: organizationSlug}
     })
     .then(res=>{
-      const {name} = res.data.organizationWithSlug;
+      console.log('res', res)
+      const {name, pictureUrl} = res.data.organizationWithSlug;
       this.setState({
         title: data.maintitle.text.replace('{{organizationName}}', name),
         description: data.description.text.replace('{{organizationName}}', name),
-      })      
+        details: data.details.text.replace('{{organizationName}}', name),
+        pictureUrl,
+      });
     })
     .catch(err=>console.error(err))
   }
   render(){
+    const { data } = this.props;
     return (
       <AppMinimal>
         <Seo
           title={this.state.title}
           description={this.state.description}
         />
-        <SectionFullPage
-          title={this.state.title}
-          subtitle={this.state.description}
-          // background={data.bkg_image.url}
-          // image={{
-          //   url: data.image.url,
-          //   width: data.image.dimensions.width,
-          //   height: data.image.dimensions.height,
-          // }}
-        >
-        </SectionFullPage>
-        <Button
-          element="button"
-          size="L"
-          shadow={false}
-          onClick={console.log('download item')}
-          >
-          Download
-        </Button>
+        <section style={{background: `url(${data.bkg_image.url}) no-repeat bottom`, backgroundSize: '100%', bottom: 0, width: '100vw', position: 'absolute', height: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', overflowX: 'hidden'}}>
+          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <img src={this.state.pictureUrl}  style={{width: '50px', height: '50px'}} />
+            <img src={data.linkbewteenorgandstationicons.url}  style={{width: 'auto', height: '30px'}} />
+            <img src={data.stationicon.url}  style={{width: '50px', height: '50px'}} />
+          </div>
+          <h1>
+            {this.state.title}
+          </h1>
+            {this.state.description}
+          <h2>
+          </h2>
+          <Button
+            style={{display: 'flex', justifyContent: 'flex-between'}}
+            element="button"
+            size="L"
+            shadow={false}
+            onClick={()=>{window.open(data.button_url.url)}}
+            >
+            <img src={data.download_icon.url}/>
+            {data.button_text}
+          </Button>
+          <p>{this.state.details}</p>
+          <img src={data.illustration.url} height={400} width={400} style={{borderRadius: '10.4px', boxShadow: '10px 10px 42px -21px rgba(0,0,0,0.75)'}} />
+        </section>
       </AppMinimal>
     )
   }
@@ -109,22 +122,37 @@ export const pageQuery = graphql`
   query welcome {
     prismicWelcome{
       data {
-        # stationicon{
-        #   image
-        # }
+        organizationicon{
+          text
+        },
+        linkbewteenorgandstationicons{
+          url
+        },
+        stationicon{
+          url
+        },
+        bkg_image{
+          url
+        },
+        details{
+          text
+        },
+        button_text,
+        button_url{
+          url
+        },
+        download_icon{
+          url
+        }
+        illustration{
+          url
+        }
         maintitle {
           text
         }
         description {
           text
         }
-        # image {
-        #   url,
-        #   dimensions {
-        #     width,
-        #     height
-        #   }
-        # }
       }
     }
   }
